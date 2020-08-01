a5a8a36d4bd4c28cd153997091725283080bab5c
irimina <irimina@gmail.com>
2020-07-31 20:56:13 -0400

    Create buldTranslatorwithDicts#4.py

---
+++ buldTranslatorwithDicts#4.py
@@ -0,0 +1,42 @@
+
+'''
+In this lesson we are going to work through an example program that will translate words from English to Maori. 
+In this code we will use both a dictionary and functions.
+
+The next lesson will step through some testing and some advice for commenting your code.
+
+Let's get started by building the dictionary and writing the first function.
+
+
+Word List:
+
+apple: aporo
+chair: turu
+pen: pene
+hello: kia ora
+goodbye: ka kite
+Menu Text:
+
+Type: 
+ '1' to view menu
+ '2' to see word list
+ '3' to translate a word
+ '4' to end program
+
+'''
+
+#START
+# English to Maori dictionary
+
+
+# Menu function
+
+##############################################################################
+
+
+
+
+
+
+
+
