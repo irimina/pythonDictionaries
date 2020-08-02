f0f3b800d7afc65fad7fc6d8364523b6be6c4c7e
irimina <irimina@gmail.com>
2020-08-02 16:15:19 -0400

    Update buldTranslatorwithDicts#4.py

--- buldTranslatorwithDicts#4.py
+++ buldTranslatorwithDicts#4.py
@@ -31,6 +31,84 @@ Type:
 
 # Menu function
 
+
+#END
+
+# English to Maori dictionary
+WORD_LIST= {"apple": "aporo",
+"chair": "turu",
+"pen": "pene",
+"hello": "kia ora",
+"goodbye": "ka kite"}
+
+# Menu function
+def menu():
+  print("""
+  Type: 
+ '1' to view menu
+ '2' to see word list
+ '3' to translate a word
+ '4' to end program
+  
+  """)
+  
+menu()
+##############################################################################
+'''
+Creating the function to print out the word list
+If we look at the menu text, we can see the user has 4 options. We have created the function for option 1 - view menu.
+
+Now let's create the function for option 2 - see word list. 
+This function will loop through the dictionary and print out each word and its translation.
+'''
+
+#START
+# English to Maori dictionary
+WORD_LIST = {'apple':'aporo','chair':'turu','pen':'pene','hello':'kia ora','goodbye':'ka kite'}
+
+# Menu function
+def menu():
+  print("Type: \n",
+        "'1' to view menu\n",
+        "'2' to see word list\n",
+        "'3' to translate a word\n",
+        "'4' to end program")
+
+# Show word list function
+
+
+# END
+# English to Maori dictionary
+WORD_LIST = {'apple':'aporo','chair':'turu','pen':'pene','hello':'kia ora','goodbye':'ka kite'}
+ 
+# Menu function
+def menu():
+    print("Type: \n",
+          "'1' to view menu\n",
+          "'2' to see word list\n",
+          "'3' to translate a word\n",
+          "'4' to end program")
+ 
+# Show word list function
+def show_list():
+    for word, translation in WORD_LIST.items():
+        print(word.title(), translation))
+ 
+show_list()
+
+
+##############################################################################
+
+
+
+
+
+
+##############################################################################
+
+
+
+
 ##############################################################################
 
 
